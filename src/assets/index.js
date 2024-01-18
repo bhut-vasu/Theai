4174853e92557726a432eac8b48b805643a95e51
Bhut Vasu <87259010+bhut-vasu@users.noreply.github.com>
2024-01-18 21:27:35 +0530

    Ready To Fire

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import linkIcon from './link.svg'
+import loader from './loader.svg'
+import copy from './favicon.svg'
+import logo from './logo.png'
+import tick from './tick.svg'
+
+export {
+    linkIcon,
+    loader,
+    copy,
+    logo,
+    tick
+}
